43a2a38bb30706fb137b7f694b6761cdd7765868
tranvanhung2003 <tranvanhan29042007@gmail.com>
2025-03-21 10:40:42 +0700

    Time: 21 ms (87.33%) | Space: 66 MB (6.48%) | Easy | .js

---
+++ 1603-design-parking-system/1603-design-parking-system.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number} big
+ * @param {number} medium
+ * @param {number} small
+ */
+var ParkingSystem = function (big, medium, small) {
+  this.arr = [big, medium, small];
+};
+
+/**
+ * @param {number} carType
+ * @return {boolean}
+ */
+ParkingSystem.prototype.addCar = function (carType) {
+  if (this.arr[carType - 1] > 0) {
+    this.arr[carType - 1]--;
+
+    return true;
+  } else {
+    return false;
+  }
+};
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * var obj = new ParkingSystem(big, medium, small)
+ * var param_1 = obj.addCar(carType)
+ */
