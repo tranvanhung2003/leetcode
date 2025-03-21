e008abbdedc168de7c1d8d314985601f35fa6890
tranvanhung2003 <tranvanhan29042007@gmail.com>
2025-03-21 10:38:51 +0700

    Time: 2 ms (70.49%) | Space: 56.1 MB (87.12%) | Easy | .js

---
+++ 3467-transform-array-by-parity/3467-transform-array-by-parity.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var transformArray = function (nums) {
+  let count = 0;
+
+  nums.forEach((val) => {
+    if (val % 2 === 0) {
+      ++count;
+    }
+  });
+
+  return Array(count)
+    .fill(0)
+    .concat(Array(nums.length - count).fill(1));
+};
