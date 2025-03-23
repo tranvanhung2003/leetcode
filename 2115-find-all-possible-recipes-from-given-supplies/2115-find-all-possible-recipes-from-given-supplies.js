bcf0dae6feccda5fec9621b4166cd59b9cda61cf
tranvanhung2003 <tranvanhan29042007@gmail.com>
2025-03-23 12:42:20 +0700

    Time: N/A (0%) | Space: N/A (0%) | Medium | .js

---
+++ 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.js
@@ -0,0 +1,60 @@
+/**
+ * @param {string[]} recipes
+ * @param {string[][]} ingredients
+ * @param {string[]} supplies
+ * @return {string[]}
+ */
+var findAllRecipes = function (recipes, ingredients, supplies) {
+  const setSupplies = new Set(supplies);
+  const setYes = new Set(),
+    setNo = new Set();
+  const map = new Map();
+
+  recipes.forEach((val, idx) => map.set(val, ingredients[idx]));
+
+  function hasRecipe(recipe, set) {
+    if (set.has(recipe)) {
+      setNo.add(recipe);
+
+      return false;
+    } else {
+      set.add(recipe);
+    }
+
+    if (setYes.has(recipe)) return true;
+
+    if (setNo.has(recipe)) return false;
+
+    if (!recipes.includes(recipe)) {
+      setNo.add(recipe);
+
+      return false;
+    }
+
+    const ingredientList = map.get(recipe);
+
+    for (let ingredient of ingredientList) {
+      if (hasIngredient(ingredient)) {
+        continue;
+      }
+
+      if (!hasRecipe(ingredient, set)) {
+        setNo.add(ingredient);
+
+        return false;
+      } else {
+        setYes.add(ingredient);
+      }
+    }
+
+    setYes.add(recipe);
+
+    return true;
+  }
+
+  function hasIngredient(ingredient) {
+    return setSupplies.has(ingredient) ? true : false;
+  }
+
+  return recipes.filter((val) => hasRecipe(val, new Set()));
+};
